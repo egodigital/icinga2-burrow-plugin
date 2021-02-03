2b0723399ed2c7b3e9fa165cf37465ee71cc0e60
Eric Kesseler <eric.kesseler@e-go-digital.com>
2021-02-03 13:16:27 +0100

    GPL3 license

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /**
- * This file is part of the icinga2-burrow-plugin distribution.
- * Copyright (c) e.GO Digital GmbH, Aachen, Germany (https://www.e-go-digital.com/)
+ * icinga2-burrow-plugin
+ * Copyright (C) 2020  e.GO Digital GmbH, Aachen, Germany
  *
- * icinga2-burrow-plugin is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, version 3.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
  *
- * icinga2-burrow-plugin is distributed in the hope that it will be useful, but
+ * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
